6a16eddef74c1c8af3323e74c58b5317d7817b3c
shtnko <80770421+shtnko@users.noreply.github.com>
2025-07-06 17:55:34 +0200

    Delete 2 part/2 part directory

--- 2 part/2 part/phonebook/src/components/Person.jsx
+++
@@ -1,5 +0,0 @@
-
-const Person = ({ person, removePerson }) => {
-  return <li> Name: {person.name}, Number: {person.number} <button onClick={removePerson} >remove</button> </li>}
-
-export default Person
